afa1e4c62a46aa68ac3700697c0d2a8e6101e032
cursebearer <rafa.pavesipassos@gmail.com>
2025-03-22 13:41:49 -0300

    feat: update/delete user

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -28,3 +28,51 @@ export const getAllUsers = async (req, res) => {
       res.status(400).json(err);
   }
 };
+
+export const getUserById = async (req, res) => {
+
+    try {
+        const user = await User.findByPk(req.params.id);
+
+        res.status(200).json({ user });
+   } catch (err) {
+        res.status(400).json(err);
+   }
+};
+
+export const updateUser = async (req, res) => {
+
+    try {
+        const user = await User.update(
+            {
+                name: req.body.name,
+                age: req.body.age,
+                email: req.body.email,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+    
+        res.status(200).json({ user });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+export const deleteUser = async (req, res) => {
+
+    try {
+     const user = await User.destroy({
+         where: {
+             id: req.params.id,
+         },
+         });
+     
+       res.status(200).json({ user });
+    } catch (err) {
+     res.status(400).json(err);
+    }
+ };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createUser, getAllUsers } from '../controllers/UserController.js';
+import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.post('/cadastro', createUser);
-
 router.get('/usuarios', getAllUsers); 
+router.get('/usuario/:id', getUserById);
+router.put('/usuario/:id', updateUser); 
+router.delete('/usuario/:id', deleteUser);
 
 
 export default router;
