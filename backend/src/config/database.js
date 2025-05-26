0d7b3525bd1da57ccb480daf4eaad7d383d906ba
cursebearer <rafa.pavesipassos@gmail.com>
2025-05-26 00:02:57 -0300

    feat: criando banco de teste

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,12 @@
+const isTest = process.env.NODE_ENV === 'test';
+ // console.log('Conectando ao banco:', isTest ? 'healthenv_test' : 'healthenv');
+
 export default {
   dialect: "postgres",
   host: "localhost",
+  port: isTest ? 5433 : 5432,
   logging: false,
   username: "healthenv",
   password: "healthenv",
-  database: "healthenv",
+  database: isTest ? "healthenv_test" : "healthenv",
 };
